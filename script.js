350c168c28561bcb75d742a2806b8a6378f6e019
Ben <bntgion@gmail.com>
2022-09-19 00:44:35 -0400

    add playRound() function

--- script.js
+++ script.js
@@ -10,7 +10,30 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+    switch(playerSelection + "|" + computerSelection) {
+        case "rock|rock":
+            return "Tie! You both chose Rock!";
+        case "rock|paper":
+            return "You Lose! Paper beats Rock";
+        case "rock|scissors":
+            return "You Win! Rock beats Scissors!";
+        case "paper|rock":
+            return "You Win! Paper beats Rock!";
+        case "paper|paper":
+            return "Tie! You both chose Rock!";
+        case "paper|scissors":
+            return "You Lose! Scissors beats Paper!";
+        case "scissors|rock":
+            return "You Lose! Rock beats Scissors!";
+        case "scissors|paper":
+            return "You Win! Scissors beats paper!";
+        case "scissors|scissors":
+            return "Tie! You both chose Rock!";
+        default:
+            return "Invalid User Input";
+    }
 }
 
 const playerSelection = "rock";
